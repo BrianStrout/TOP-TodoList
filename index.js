c1a079b25a6eb0e0188d701e87e7ea91be07ca52
Brian J. Strout <brianjstrout@gmail.com>
2022-09-02 11:34:47 +0100

    got accordion reactivity in tasks functioning

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.onload = (e) => {
-    console.log("main JS script firing");
+
 }
 
 let projectList=[];
@@ -13,7 +13,6 @@ function generateAtmosphere(){
 }
 
 function generateNoteBook(){
-   console.log("notes fired")
    let noteBook = document.createElement('div');
    noteBook.classList.add('noteBook');
    atmosphere.appendChild(noteBook); 
@@ -30,7 +29,7 @@ function generateNoteBook(){
 }
 
 function generateNewListItem(itemTitle, itemPriority, itemDueDate, itemDescription, itemHasNotes, itemHasChecklist){
-    console.log("new item");
+    // console.log("new item");
     // console.table(title, dueDate, priority, description, hasNotes, hasChecklist);
     let newItemObject ={title: itemTitle,
                         priority: itemPriority,
@@ -39,10 +38,10 @@ function generateNewListItem(itemTitle, itemPriority, itemDueDate, itemDescripti
                         notes: itemHasNotes,
                         checkList: itemHasChecklist,
                         listIndex: undefined,
+                        inputIndex: projectList.length,
                         creationDate: undefined,
                         }
 
-    console.log(newItemObject);
     addItemToList(newItemObject);
     publishToList(newItemObject);
 }
@@ -52,44 +51,55 @@ function addItemToList(newToDo){
 }
 
 function publishToList(newItem){
+    
     let noteBookList = document.getElementById('noteBookList');
+
     let listItemCard = document.createElement('div');
+
     listItemCard.classList.add('listItemCard');
+    
     noteBookList.appendChild(listItemCard);
 
     let listItemTitle= document.createElement('div');
     listItemTitle.classList.add('listItemTitle');
     listItemTitle.classList.add('listItemItem');
     listItemTitle.innerHTML = "<h4 style='display: inline; border-bottom: 1px solid red;'> Task: </h4>" + newItem.title;
+    listItemTitle.dataset.dewey=newItem.inputIndex;
     listItemCard.appendChild(listItemTitle);
+
     if (newItem.priority === "high"){ console.log("HIGH REIGSTERED!")}
+
+    let listItemCardContent= document.createElement('div');
+    listItemCardContent.classList.add('listItemContent');
+    listItemCardContent.classList.add('listItemContent--active');
+    listItemCardContent.classList.add('listItemItem');
+    listItemCard.appendChild(listItemCardContent);
 
     let listItemDueDate = document.createElement('div');
     listItemDueDate.classList.add('listItemDate');
     listItemDueDate.classList.add('listItemItem');
     listItemDueDate.innerHTML = "<h6 style='display: inline'> Due: </h4>" + newItem.date;
-    listItemCard.appendChild(listItemDueDate);
+    listItemCardContent.appendChild(listItemDueDate);
 
     let listItemDescription = document.createElement('div');
     listItemDescription.classList.add('listItemDescription');
     listItemDescription.classList.add('listItemItem');
     listItemDescription.innerHTML = "<h6 style='display: inline'> Description: </h4>" + newItem.description;
-    listItemCard.appendChild(listItemDescription);
+    listItemCardContent.appendChild(listItemDescription);
 
     if (newItem.notes==="yes"){
     let listItemNotes = document.createElement('div');
     listItemNotes.classList.add('listItemNotes');
     listItemNotes.classList.add('listItemItem');
     listItemNotes.innerHTML = "<h6 style='display: inline'> Notes: </h4><input placeholder='...'value=''>";
-    listItemCard.appendChild(listItemNotes);
+    listItemCardContent.appendChild(listItemNotes);
     }
-
     if (newItem.checkList==="yes"){
         let listItemCheckList = document.createElement('div');
         listItemCheckList.classList.add('listItemDescription');
         listItemCheckList.classList.add('listItemItem');
         listItemCheckList.innerHTML = "<ol><li></li></ol>";
-        listItemCard.appendChild(listItemCheckList);
+        listItemCardContent.appendChild(listItemCheckList);
         }
     
     let listIndex = projectList.length;
@@ -97,12 +107,11 @@ function publishToList(newItem){
     metaDiv.classList.add('listItemItem');
     metaDiv.classList.add('listMetaDiv');
         metaDiv.textContent=`index: ${listIndex}`;
-        listItemCard.appendChild(metaDiv);
+        listItemCardContent.appendChild(metaDiv);
 
+        listItemCardContent.setAttribute('id', `liccNum${newItem.inputIndex}`);
+        // console.log(listItemCardContent);
 
-    // for(let i=0; i<projectList.length; i++){
-
-    // }
 }
 
 function updateIndexing(){
@@ -114,9 +123,45 @@ function test(){
     generateNoteBook();
     generateNewListItem("Task Number 1", "high", "November 1st", "Please do this already", "yes", "yes");
 }
-
 function test2(){
     generateNewListItem("Task Number 2", "high", "November 2nd", "Come on! Please do this already", "yes", "yes");
 }
 
+
+document.addEventListener('click',function(e){
+
+
+    if(e.target.classList[0]=== 'listItemTitle'){
+        let target = e.target;
+        let targetDewey=e.target.dataset.dewey;
+        console.log(targetDewey);
+        let targetDiv = document.getElementById(`liccNum${targetDewey}`);
+        targetDiv.classList.toggle('listItemContent--active');
+        console.log(targetDiv.classList);
+            if (targetDiv.classList.contains('listItemContent--active')){
+
+                targetDiv.style.maxHeight = targetDiv.scrollHeight + 'px';
+            }else{
+                targetDiv.style.maxHeight = '0px';
+            }
+
+
+        // let targetParent= target.parentElement;
+        // console.log(targetParent)
+
+    //     let targetSibling = targetParent.getElementsByClassName('listItemContent');
+    //    console.log(targetSibling[0]);
+    //     let tt = targetSibling[0]
+        // tt.setAttribute('id', 'tt');
+        // let ttt = document.getElementById('tt');
+        // ttt.style.backgroundColor = 'pink';
+        // console.log(ttt);
+        // target.classList.toggle('listItemTitle--active');
+        //     if (target.classList.contains('listItemTitle--active')){
+        //         return;
+        //     }
+
+    }
+    });
+     
 generateAtmosphere();
