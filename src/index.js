2b82e27076cb00ee179c67f83110ecc46191aac7
Brian J. Strout <brianjstrout@gmail.com>
2022-09-06 12:40:07 +0100

    transferred generate new items input to module.

--- src/index.js
+++ src/index.js
@@ -1,46 +1,17 @@
-// import defaultExport from '/src/modules/onLoad';
-import generateAtmosphere from './modules/onLoad';
-import generateNoteBook from './modules/onLoad';
+import {generateAtmosphere, generateNoteBook} from './modules/onLoad';
+import {generateInputForNewItems} from './modules/todomodule-inputnew';
 
-window.onload = (e) => {
-    generateAtmosphere();
-    generateNoteBook();
-}
-
-let projectList=[];
-
-// function generateAtmosphere(){
-//     let bodyDiv = document.getElementById("bodyDiv")
-//     let atmosphere= document.createElement('div')
-//         atmosphere.classList.add('container');
-//         atmosphere.setAttribute('id', 'atmosphere');
-//         bodyDiv.appendChild(atmosphere);
+// window.onload = (e) => {
+//     generateAtmosphere();
+//     generateNoteBook();
 // }
+generateAtmosphere();
+generateNoteBook();
 
-// function generateNoteBook(){
-//     let atmosphere = document.getElementById('atmosphere');
-//     let noteBook = document.createElement('div');
-//         noteBook.classList.add('noteBook');
-//         atmosphere.appendChild(noteBook); 
-//     let noteBookHeader = document.createElement('div');
-//         noteBookHeader.classList.add('inBook');
-//         noteBookHeader.classList.add('noteBookHeader');
-//         noteBookHeader.textContent="To Do List To Done List";
-//         noteBook.appendChild(noteBookHeader);
-//     let noteBookButtonDiv = document.createElement('div');
-//         noteBookButtonDiv.classList.add('inBook');
-//         noteBookButtonDiv.classList.add('noteBookButtonDiv');
-//         noteBook.appendChild(noteBookButtonDiv);   
-//     let noteBookButton = document.createElement('button');
-//         noteBookButton.setAttribute('id', 'newButton');
-//         noteBookButton.innerText="+";
-//         noteBookButtonDiv.appendChild(noteBookButton);
-//     let noteBookList = document.createElement('div');
-//         noteBookList.classList.add('inBook');
-//         noteBookList.classList.add('noteBookList');
-//         noteBookList.setAttribute('id', 'noteBookList');
-//         noteBook.appendChild(noteBookList);
-// }
+const projectList=[];
+
+console.log(projectList);
+console.log(projectList.length);
 
 
 
@@ -65,130 +36,6 @@ window.localStorage.setItem('user', JSON.stringify(person));
 }
 function retrieveStorage(){
     //not sure yet
-}
-
-
-
-function generateInputForNewItems(){
-    let listCount=1;
-
-    let inputItemCard = document.createElement('div');
-        inputItemCard.classList.add('inputItemCard');
-        inputItemCard.classList.add('listItemItem');
-        inputItemCard.setAttribute('id', 'inputFormCard');
-        noteBookList.appendChild(inputItemCard);
-
-    let inputItemCardTitle= document.createElement('div');
-        inputItemCardTitle.classList.add('inputItemCardTitle');
-        inputItemCardTitle.classList.add('listItemItem');
-        inputItemCardTitle.innerHTML = "<h4 style='display: inline;'> Task: </h4><input id='input1' type='text'></input>";
-        inputItemCard.appendChild(inputItemCardTitle);
-
-
-    let inputItemCardPriority = document.createElement('div'); 
-    inputItemCardTitle.classList.add('inputItemCardPriority');
-    inputItemCardTitle.classList.add('listItemItem');
-    inputItemCard.appendChild(inputItemCardTitle);    
-
-
-
-    let radioDiv = document.createElement('div');
-        radioDiv.classList.add('inputPriorityDiv');
-        radioDiv.classList.add('listItemItem');
-        inputItemCard.appendChild(radioDiv); 
-
-    let inputItemCardPriorityLeg = document.createElement ('label');
-        inputItemCardPriorityLeg.innerHTML="Priority: ";
-        // inputItemCardPriorityLeg.setAttribute('float', 'left');
-        radioDiv.appendChild(inputItemCardPriorityLeg); 
-                let inputItemCardPriorityBut1 = document.createElement ('input');
-                    inputItemCardPriorityBut1.setAttribute('type', 'radio');
-                    inputItemCardPriorityBut1.setAttribute('id', 'highPrio' )
-                    inputItemCardPriorityBut1.setAttribute('value', 'high');
-                    inputItemCardPriorityBut1.setAttribute('name', 'prioritylevel');
-                    radioDiv.appendChild(inputItemCardPriorityBut1);    
-                let inputItemCardPriorityBut1L = document.createElement ('label');
-                    inputItemCardPriorityBut1L.setAttribute('value', 'high');
-                    inputItemCardPriorityBut1L.innerHTML="<label for='highPrio'>High</label>"
-                    radioDiv.appendChild(inputItemCardPriorityBut1L);
-                let inputItemCardPriorityBut2 = document.createElement ('input');
-                    inputItemCardPriorityBut2.setAttribute('type', 'radio');
-                    inputItemCardPriorityBut2.setAttribute('id', 'medPrio' )
-                    inputItemCardPriorityBut2.setAttribute('value', 'high');
-                    inputItemCardPriorityBut2.setAttribute('name', 'prioritylevel');
-                    radioDiv.appendChild(inputItemCardPriorityBut2);    
-                let inputItemCardPriorityBut2L = document.createElement ('label');
-                    inputItemCardPriorityBut2L.setAttribute('value', 'high');
-                    inputItemCardPriorityBut2L.innerHTML="<label for='medPrio'>Med</label>"
-                    radioDiv.appendChild(inputItemCardPriorityBut2L);    
-                let inputItemCardPriorityBut3 = document.createElement ('input');
-                    inputItemCardPriorityBut3.setAttribute('type', 'radio');
-                    inputItemCardPriorityBut3.setAttribute('id', 'lowPrio' )
-                    inputItemCardPriorityBut3.setAttribute('value', 'high');
-                    inputItemCardPriorityBut3.setAttribute('name', 'prioritylevel');
-                    radioDiv.appendChild(inputItemCardPriorityBut3);    
-                let inputItemCardPriorityBut3L = document.createElement ('label');
-                    inputItemCardPriorityBut3L.setAttribute('value', 'high');
-                    inputItemCardPriorityBut3L.innerHTML="<label for='lowPrio'>Low</label>"
-                    radioDiv.appendChild(inputItemCardPriorityBut3L);
-
-    let inputItemDueDate = document.createElement('div');
-        inputItemDueDate.classList.add('inputItemDate');
-        inputItemDueDate.classList.add('inputItemItem');
-        inputItemDueDate.innerHTML = "<h6 style='display: inline'> Due: </h4><input id='input3' type='text'></input>";
-        inputItemCard.appendChild(inputItemDueDate);
-
-    let inputItemDescription = document.createElement('div');
-        inputItemDescription.classList.add('inputItemDescription');
-        inputItemDescription.classList.add('inputItemItem');
-        inputItemDescription.innerHTML = "<h6 style='display: inline'> Description: </h4><input id='input4' type='text'></input>";
-        inputItemCard.appendChild(inputItemDescription);
-
-    let inputItemNotesCheck = document.createElement('div');
-        inputItemNotesCheck.classList.add('inputItemNotes');
-        inputItemNotesCheck.classList.add('inputItemItem');
-        inputItemNotesCheck.innerHTML = "<h6 style='display: inline'>Want room for notes? </h4><input id='input5' type='checkbox'></input>";
-        inputItemCard.appendChild(inputItemNotesCheck);
-
-    let inputItemCheckInput = document.createElement('div');
-        inputItemCheckInput.classList.add('inputItemCheckList');
-        inputItemCheckInput.classList.add('inputItemItem');
-        inputItemCheckInput.innerHTML = "<h6 style='display: inline'>Want a checklist? </h4><input id='input6' type='checkbox'></input>";
-        inputItemCard.appendChild(inputItemCheckInput);
-
-    let inputItemNotes = document.createElement('div');
-        inputItemNotes.classList.add('inputItemNotes');
-        inputItemNotes.classList.add('inputItemItem');
-        inputItemNotes.classList.add('hidden');
-        inputItemNotes.setAttribute('id', 'inputNoteField');
-        inputItemNotes.innerHTML = "<h6 style='display: inline'>Notes: </h4><input id='input7' type='text' id='notesUserInput'></input>";
-        inputItemCard.appendChild(inputItemNotes);
-
-    let inputtingItemCheckInput = document.createElement('div');
-        inputtingItemCheckInput.classList.add('listItemCheckList');
-        inputtingItemCheckInput.classList.add('listItemItem');
-        inputItemCard.appendChild(inputtingItemCheckInput);
-
-    let inputtingItemCheckOL = document.createElement('ul');
-        inputtingItemCheckInput.appendChild(inputtingItemCheckOL);
-    let makeAnotherLI = document.createElement('li');
-        inputtingItemCheckOL.appendChild(makeAnotherLI);
-    let makeAnotherLIinput = document.createElement('input');
-        makeAnotherLIinput.classList.add('checkListItem');
-        inputtingItemCheckOL.appendChild(makeAnotherLIinput);
-
-
-    let newToDoSubmitDiv =  document.createElement('div');
-        newToDoSubmitDiv.classList.add('inputItemSubmit');
-        newToDoSubmitDiv.classList.add('inputItemItem');
-        inputItemCard.appendChild(newToDoSubmitDiv);
-    let newToDoSubmitButton =  document.createElement('button');
-        newToDoSubmitButton.textContent="submit";
-        newToDoSubmitButton.setAttribute('id', 'newToDoSubmitButton');
-        newToDoSubmitDiv.appendChild(newToDoSubmitButton);
-        
-
-
 }
 
 
